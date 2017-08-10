e284eaec78d97ce19f27a60ce4192203f90019c3
Hifza Sakhi <hifzasakhi@gmail.com>
2017-08-10 00:26:50 -0400

    created makefile to fetch image url

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+fbpost:
+	curl localhost:8000/facebook -X POST -H "Content-Type: application/json" -d @example.json
--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from flask import Flask, request
 import io
 import urllib2 as urllib 
 import json
+from PIL import Image
+import io
+from urllib2 import urlopen
+from PIL import Image
+from cStringIO import StringIO
+      
+
 
 app = Flask(__name__)
 
@@ -16,7 +23,7 @@ def helloHifza():
 
 @app.route('/img/<url>/',methods=['GET'])
 def getImage(url):
-  print url
+  print(request.args)
   #fd = urllib.urlopen(url)
   #image_file = io.BytesIO(fd.read())
   #img = Image.open(image_file)
@@ -28,15 +35,63 @@ def getImage(url):
     #return height, width
     #dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
 
+
+
 @app.route('/size',methods=['POST'])
-def getSize():
+def getSize_old():
   #url="localhost:5001/size"
   data = request.get_json()
-  #print json
+  #print whole json payloaf
   print(data)
-  #print(data[0])
-  # r = requests.get('http://httpbin.org/get')  #r = requests.get(url, headers=my_headers, params=payload)
+  #pulls the URL out of the json payload
+  image_url = data['url']
+
+  print(image_url)
+
+  #image_byt = urlopen(image_url)
+
+  img_file = urllib.urlopen(image_url)
+  im = StringIO(img_file.read())
+  img = Image.open(im)
+  #print(img.size)
+  #image_b46 = base64.encodestring(image_byt)
+
+  #img = Image.open(image_byt)
+  print(height)
+  img.show()
+ 
   return "{}\n"
 
+@app.route('/facebook',methods=['POST'])
+def facebook():
+  #url="localhost:5001/size"
+  data = request.get_json()
+  #print whole json payloaf
+  print(data)
+  #pulls the URL out of the json payload
+  image_url = data['entry'][0]['message']['attachments'][0]['payload']['url']
+
+  print(image_url)
+
+  #image_byt = urlopen(image_url)
+
+  # img_file = urllib.urlopen(image_url)
+  # im = StringIO(img_file.read())
+  # img = Image.open(im)
+  # #print(img.size)
+  # #image_b64 = base64.encodestring(image_byt)
+
+  # #img = Image.open(image_byt)
+  # print(height)
+  # img.show()
+ 
+  return image_url
+
+@app.route('/sizer',methods=['POST'])
+def getSize():
+  print("Hello")
+
+
+#
 if __name__ == "__main__":
-    app.run(port=5001)
+  app.run(port=8000)
