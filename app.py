df817de8d6eb9df9c7ed78006bef9e047d196c72
Hifza Sakhi <hifzasakhi@gmail.com>
2017-08-29 00:41:02 -0400

    Incorporated error handling into the application

--- app.py
+++ app.py
@@ -17,55 +17,8 @@ from urllib2 import urlopen
 from PIL import Image
 from cStringIO import StringIO
       
-
-
 app = Flask(__name__)
-
-
-@app.route("/hifza")
-def helloHifza():
-    return "Hello Hifza!"
-
-@app.route('/img/<url>/',methods=['GET'])
-def getImage(url):
-  print(request.args)
-  #fd = urllib.urlopen(url)
-  #image_file = io.BytesIO(fd.read())
-  #img = Image.open(image_file)
-  return "cool"    
-   #response = requests.get(url)
-    #img = Image.open(BytesIO(response.content))
-  #return img    
-#height, width = img.size
-    #return height, width
-    #dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
-
-
-
-@app.route('/size',methods=['POST'])
-def getSize_old():
-  #url="localhost:5001/size"
-  data = request.get_json()
-  #print whole json payloaf
-  print(data)
-  #pulls the URL out of the json payload
-  image_url = data['url']
-
-  print(image_url)
-
-  #image_byt = urlopen(image_url)
-
-  img_file = urllib2.urlopen(image_url)
-  im = StringIO(img_file.read())
-  img = Image.open(im)
-  #print(img.size)
-  #image_b46 = base64.encodestring(image_byt)
-
-  #img = Image.open(image_byt)
-  print(height)
-  img.show()
- 
-  return "{}\n"
+db = None
 
 def retrieveImageName(image_url):
   filename = image_url.split('/')[-1].split('.')[0]
@@ -124,68 +77,53 @@ def fetchLyricsFromDB(key_string):
 
   #gets the lyrics associated with this particular key
   lyrics_array = lyrics_db.find_one({"key": key_string}, {"lyrics":1})
+  return lyrics_array['lyrics']
 
-  return lyrics_array
-
-#@app.route('/temp',methods=['GET'])
 def renderTemplate(lyrics_array):
   print("trying to render template")
   return render_template("template.html",lyrics=lyrics_array)
 
-@app.route('/facebook',methods=['GET','POST'])
+@app.route('/facebook',methods=['POST'])
 def facebook():
 
   data = request.get_json()
-  #print whole json payload
-  print(data)
-  #pulls the URL out of the json payload
-  image_url = data['entry'][0]['message']['attachments'][0]['payload']['url']
+  if data == None:
+    return renderTemplate(None)
 
-  print(image_url)
+  image_url = None
+  if data != None: 
+    #print whole json payload if present
+    print(data)
+    #pulls the URL out of the json payload
+    image_url = data['entry'][0]['message']['attachments'][0]['payload']['url']
+    print(image_url)
 
-  imageName = downloadImage(image_url)
+  imageName = None
+  if image_url != None:   
+    imageName = downloadImage(image_url)
   #urllib.urlretrieve(image_url, "/tmp/hifzaFlaskApp/" + retrieveImageName(image_url) + retrieveImageExtension(image_url))
 
-  lyrics = generateLyrics(imageName)
+  lyrics = None
+  key_string = None
+  if imageName != None:
+    lyrics = generateLyrics(imageName)
+    key_string = generateKeyString()
+    print(key_string)
 
-  key_string = generateKeyString()
-
-  #db = getDBConnection()
-
-  insertIntoDB(key_string, lyrics)
-
-  print("Getting lyrics from db")
-  lyrics_array_from_db = fetchLyricsFromDB(key_string)
-  print("Lyrics from db are: ")
-  print(lyrics_array_from_db)
-
-  renderTemplate(lyrics)
+  if key_string != None:
+    insertIntoDB(key_string, lyrics)
 
   return ("",200)
 
+@app.route('/lyrics/<pageid>',methods=['GET'])
+def getPage(pageid=None):
+  print("Getting lyrics from db")
+  lyrics = fetchLyricsFromDB(pageid)
+  print("Lyrics from db are: ")
+  print(lyrics)
+  return renderTemplate(lyrics)
 
-  #image_byt = urlopen(image_url)
-
-  # img_file = urllib.urlopen(image_url)
-  # im = StringIO(img_file.read())
-  # img = Image.open(im)
-  # #print(img.size)
-  # #image_b64 = base64.encodestring(image_byt)
-
-  # #img = Image.open(image_byt)
-  # print(height)
-  # img.show()
- 
-  #return image_url
-
-
-  #return 200 OK
-
-@app.route('/sizer',methods=['POST'])
-def getSize():
-  print("Hello")
-
-
-#
 if __name__ == "__main__":
+  #db = mongo crap 
+
   app.run(port=8000,debug=True)
